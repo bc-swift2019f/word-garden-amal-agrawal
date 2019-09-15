aedfec9bcc6fda3cb8bd4b313c6ad1bdcd9b47f1
Amal Agrawal <amalagrawal@amals-macbook-pro-2.local>
2019-09-15 14:41:42 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Amal Agrawal on 9/15/19.
+//  Copyright © 2019 Amal Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
